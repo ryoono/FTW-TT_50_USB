e64e776f3f0b0778e2aafb01db6f520ef71e8fad
k16025kk <ono@pluslab.org>
2023-04-09 22:16:24 +0900

    コマンド受け取る処理実装

--- 模索/solenoidOperationCheck/solenoidOperationCheck.ino
+++ 模索/solenoidOperationCheck/solenoidOperationCheck.ino
@@ -1,9 +1,66 @@
-void setup() {
-  // put your setup code here, to run once:
 
+#define MOVE_SINGLE   0 // 単発稼働
+#define MOVE_CONTINUE 1 // 連続稼働
+
+bool startFlag; // 稼働状態 false:停止 / true:起動
+char startType; // 稼働タイプ
+int  onTime;    // ソレノイドのON時間
+int  offTime;   // ソレノイドのOFF時間
+
+void setup() {
+
+  startFlag = false;        // 停止に指定
+  startType = MOVE_SINGLE;  // 単発に指定
+
+  onTime  = 1000;
+  offTime = 1000;
+
+  pinMode( 13, OUTPUT);
+  pinMode(  4, OUTPUT);
+  digitalWrite( 13, LOW);   // 眩しいからLED消しておく
+  digitalWrite(  4, LOW);
+
+  Serial.begin( 9600 );
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  // 何らかのデータを受信した場合の処理
+  if ( Serial.available() > 0 ) {
+    String dt = Serial.readStringUntil('\n');
+
+    // ##################################
+    // ####---- コマンド受信時の処理 ----####
+    
+    // ソレノイドのON時間を指定するコマンド受信時
+    if( dt.indexOf("on") == 0 ){
+      String num_s = dt.substring( 3, dt.length());
+      
+    }
+    // ソレノイドのOFF時間を指定するコマンド受信時
+    else if( dt.indexOf("off") == 0 ){
+      String num_s = dt.substring( 4, dt.length());
+    }
+    // 連続稼働コマンド受信時
+    else if( dt.compareTo("continue") == 0 ){
+      startType = MOVE_CONTINUE;
+    }
+    // 単発稼働コマンド受信時
+    else if( dt.compareTo("single") == 0 ){
+      startType = MOVE_SINGLE;
+    }
+    // 開始コマンド受信時
+    else if( dt.compareTo("start") == 0 ){
+      startFlag = true;
+    }
+    // 停止コマンド受信時
+    else if( dt.compareTo("s") == 0 ){
+      startFlag = false;
+    }
+  }
+
+  digitalWrite( 4, LOW);
+  delay(40);
+  digitalWrite( 4, HIGH);
+  delay(40);
 }
