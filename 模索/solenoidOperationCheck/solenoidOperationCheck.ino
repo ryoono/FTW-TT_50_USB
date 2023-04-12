1bd26c669dd5b83b1b8175384460d824f9afb050
k16025kk <ono@pluslab.org>
2023-04-12 18:54:46 +0900

    動かなかった箇所の修正

--- 模索/solenoidOperationCheck/solenoidOperationCheck.ino
+++ 模索/solenoidOperationCheck/solenoidOperationCheck.ino
@@ -2,6 +2,8 @@
 #define MOVE_SINGLE   0 // 単発稼働
 #define MOVE_CONTINUE 1 // 連続稼働
 
+#define SOL_PIN 4 // ソレノイド制御用ピン番号
+
 bool startFlag; // 稼働状態 false:停止 / true:起動
 char startType; // 稼働タイプ
 int  onTime;    // ソレノイドのON時間[ms]
@@ -15,12 +17,11 @@ void setup() {
   onTime  = 1000; // 初期値は1000[ms]
   offTime = 1000;
 
-  pinMode( 13, OUTPUT);
-  pinMode(  4, OUTPUT);
-  digitalWrite( 13, LOW);   // 眩しいからLED消しておく
-  digitalWrite(  4, LOW);
+  pinMode( SOL_PIN, OUTPUT);
+  digitalWrite( SOL_PIN, LOW);
 
   Serial.begin( 9600 );
+  Serial.println("コマンド詳細はREADME.mdを参照してください");
   Serial.println("例外処理など入れてないため，コマンドを間違えないように注意してください");
 }
 
@@ -28,35 +29,37 @@ void loop() {
 
   // 何らかのデータを受信した場合の処理
   if ( Serial.available() > 0 ) {
-    String dt = Serial.readStringUntil('\n');
+    // 終端文字によってここは変更．
+    // macだとCR+LFのため，CRまでで切る
+    String command = Serial.readStringUntil('\r');
 
     // ##################################
     // ####---- コマンド受信時の処理 ----####
     
     // ソレノイドのON時間を指定するコマンド受信時
-    if( dt.indexOf("on") == 0 ){
-      String onTime_s = dt.substring( 3, dt.length());
+    if( command.indexOf("on ") == 0 ){
+      String onTime_s = command.substring( 3, command.length());
       onTime = onTime_s.toInt();
     }
     // ソレノイドのOFF時間を指定するコマンド受信時
-    else if( dt.indexOf("off") == 0 ){
-      String offTime_s = dt.substring( 4, dt.length());
+    else if( command.indexOf("off ") == 0 ){
+      String offTime_s = command.substring( 4, command.length());
       offTime = offTime_s.toInt();
     }
     // 連続稼働コマンド受信時
-    else if( dt.compareTo("continue") == 0 ){
+    else if( command.compareTo("continue") == 0 ){
       startType = MOVE_CONTINUE;
     }
     // 単発稼働コマンド受信時
-    else if( dt.compareTo("single") == 0 ){
+    else if( command.compareTo("single") == 0 ){
       startType = MOVE_SINGLE;
     }
     // 開始コマンド受信時
-    else if( dt.compareTo("start") == 0 ){
+    else if( command.compareTo("start") == 0 ){
       startFlag = true;
     }
     // 停止コマンド受信時
-    else if( dt.compareTo("s") == 0 ){
+    else if( command.compareTo("s") == 0 ){
       startFlag = false;
     }
   }
@@ -68,9 +71,9 @@ void loop() {
   // startTypeが単発稼働ならばstartFlagをfalseにする
   if( startFlag == true ){
     
-    digitalWrite( 4, HIGH);
+    digitalWrite( SOL_PIN, HIGH);
     delay(onTime);
-    digitalWrite( 4, LOW);
+    digitalWrite( SOL_PIN, LOW);
     delay(offTime); 
     
     if( startType != MOVE_CONTINUE ){
