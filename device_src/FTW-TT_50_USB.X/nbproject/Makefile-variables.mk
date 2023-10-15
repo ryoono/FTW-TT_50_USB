c24e4ea9279ce90335ab1c2f215d96e9da526365
ryoono <ono@pluslab.org>
2023-10-15 22:26:51 +0900

    デバイス側プロジェクトファイル作成

---
+++ device_src/FTW-TT_50_USB.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=FTW-TT_50_USB.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/FTW-TT_50_USB.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=ftw-tt50usb.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/ftw-tt50usb.x.tar
